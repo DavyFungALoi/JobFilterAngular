093f3c891f9f57b611098389e446c7be780bdfc5
Davy <davyfungaloi@gmail.com>
2021-04-22 19:41:48 +0200

    added the neccesary components for the angular project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeScreenComponent } from './Screens/home-screen/home-screen.component';
+import { HeaderComponent } from './components/header/header.component';
+import { JoblistingComponent } from './components/joblisting/joblisting.component';
+import { FiltercomponentComponent } from './components/filtercomponent/filtercomponent.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeScreenComponent,
+    HeaderComponent,
+    JoblistingComponent,
+    FiltercomponentComponent
   ],
   imports: [
     BrowserModule,
