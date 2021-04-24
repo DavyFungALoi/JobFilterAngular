885a4df69a4eea7d23b9f5a72b7847a18fbc8d2a
Davy <davyfungaloi@gmail.com>
2021-04-24 20:46:35 +0200

    added an array of filtered tags

--- src/app/Screens/home-screen/home-screen.component.ts
+++ src/app/Screens/home-screen/home-screen.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { Job } from '../../models/Job';
 import jobdata from '../../../assets/data.json';
 @Component({
@@ -8,10 +8,20 @@ import jobdata from '../../../assets/data.json';
 })
 export class HomeScreenComponent implements OnInit {
   jobs: Job[];
-
+  @Input() filterArray: string[];
   constructor() {}
+  OnclickAdded(jobDetails: string): void {
+    if (this.filterArray.indexOf(jobDetails) !== -1) {
+      console.log('Value exists!');
+    } else {
+      this.filterArray.push(jobDetails);
+      console.log('added value');
+    }
+    console.log(this.filterArray)
+  }
 
   ngOnInit(): void {
     this.jobs = jobdata;
+    this.filterArray = [];
   }
 }
--- src/app/components/joblisting/joblisting.component.ts
+++ src/app/components/joblisting/joblisting.component.ts
@@ -1,17 +1,21 @@
-import { Component, OnInit, Input } from '@angular/core';
-import {Job} from "../../models/Job"
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Job } from '../../models/Job';
 
 @Component({
   selector: 'app-joblisting',
   templateUrl: './joblisting.component.html',
-  styleUrls: ['./joblisting.component.scss']
+  styleUrls: ['./joblisting.component.scss'],
 })
 export class JoblistingComponent implements OnInit {
   @Input() job: Job;
 
-  constructor() { }
+  @Output() jobDetails: EventEmitter<string> = new EventEmitter<string>();
+
+  constructor() {}
 
   ngOnInit(): void {
   }
-
+  OnFilter(jobDetails: string) {
+    this.jobDetails.emit(jobDetails);
+  }
 }
